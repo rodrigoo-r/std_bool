203832e44c8046385ca1546c73ed8932b1a47274
rodrigoo-r <2nw53j32643y@gmail.com>
2025-04-30 00:04:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# std_bool
+
+std_bool, short for standard boolean, is an extension that defines bool types,
+TRUE and FALSE constants.
+
+## License
+
+This project is licensed under the GNU GPL-3.0 License. See the [LICENSE](LICENSE) file for details.
---
+++ library.c
@@ -0,0 +1,22 @@
+/*
+* This code is distributed under the terms of the GNU General Public License.
+ * For more information, please refer to the LICENSE file in the root directory.
+ * -------------------------------------------------
+ * Copyright (C) 2025 Rodrigo R.
+ * This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+ * This is free software, and you are welcome to redistribute it
+ * under certain conditions; type `show c' for details.
+*/
+
+#include "library.h"
+
+typedef int bool;
+
+// Define true/false values
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
